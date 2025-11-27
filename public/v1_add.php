443f8e9ca231b71ced5b2e8eccbc32bc7d531ac2
Natalia Lishchenko <B00165618@mytudublin.ie>
2025-11-27 18:59:34 +0000

    Updated Version 1, added DB password, fixed mysqli connection

--- public/v1_add.php
+++ public/v1_add.php
@@ -1,25 +1,36 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "car_maintenance");
+$servername = "localhost";
+$username = "root";
+$password = "PassB62tu";
+$dbname = "car_maintenance";
+$port = 3306;
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+if (isset($_POST['service_name'])) {
     $service = $_POST['service_name'];
 
-    $stmt = $conn->prepare("INSERT INTO basic_log_v1 (service_name) VALUES (?)");
-    $stmt->bind_param("s", $service);
-    $stmt->execute();
+    $sql = "INSERT INTO basic_log_v1 (service_name) VALUES ('$service')";
+    mysqli_query($conn, $sql);
 
     echo "<p>Service added successfully!</p>";
 }
+
+
+mysqli_close($conn);
 ?>
 
 <h2>Add Service (Version 1)</h2>
 
 <form method="POST">
-    <label>Service Name:</label>
+    Service Name:
     <input type="text" name="service_name" required>
     <button type="submit">Add</button>
 </form>
--- public/v1_list.php
+++ public/v1_list.php
@@ -1,27 +1,36 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "car_maintenance");
+$servername = "localhost";
+$username = "root";
+$password = "PassB62tu";
+$dbname = "car_maintenance";
+$port = 3306;
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
-$result = $conn->query("SELECT * FROM basic_log_v1");
+
+$sql = "SELECT * FROM basic_log_v1";
+$result = mysqli_query($conn, $sql);
+
+echo "<h2>Service List (Version 1)</h2>";
+echo "<table border='1'>";
+echo "<tr><th>ID</th><th>Service Name</th></tr>";
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $id = $row['id'];
+    $name = $row['service_name'];
+
+    echo "<tr><td>$id</td><td>$name</td></tr>";
+}
+
+echo "</table>";
+
+mysqli_close($conn); 
 ?>
 
-<h2>Service List (Version 1)</h2>
-
-<table border="1">
-    <tr>
-        <th>ID</th>
-        <th>Service Name</th>
-    </tr>
-
-    <?php while ($row = $result->fetch_assoc()): ?>
-        <tr>
-            <td><?= $row['id'] ?></td>
-            <td><?= $row['service_name'] ?></td>
-        </tr>
-    <?php endwhile; ?>
-</table>
-
-<a href="v1_add.php">Add New Service</a>
+<a href='v1_add.php'>Add New Service</a>
