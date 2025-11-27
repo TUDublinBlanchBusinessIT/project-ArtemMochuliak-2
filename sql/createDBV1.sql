3e3fcd6e59e6b32257718e700d20a69bae993155
Natalia Lishchenko <B00165618@mytudublin.ie>
2025-11-27 17:00:10 +0000

    Added initial createDBV1.sql for Version 1

---
+++ sql/createDBV1.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS car_maintenance;
+USE car_maintenance;
+
+CREATE TABLE basic_log_v1 (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    service_name VARCHAR(100) NOT NULL
+);
