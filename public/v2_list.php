6d382ad2cf7cef50bbbd4e39233706c4af2fb5fa
Natalia Lishchenko <B00165618@mytudublin.ie>
2025-11-28 01:39:17 +0000

    Added HTML table layout to v2_list.php for displaying service name, cost and date

--- public/v2_list.php
+++ public/v2_list.php
@@ -15,3 +15,26 @@ $sql = "SELECT * FROM basic_log_v2";
 $result = mysqli_query($conn, $sql);
 
 ?>
+
+<h2>Service List (Version 2)</h2>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Service Name</th>
+        <th>Cost (€)</th>
+        <th>Date</th>
+    </tr>
+
+    <?php while ($row = mysqli_fetch_assoc($result)): ?>
+        <tr>
+            <td><?= $row['id'] ?></td>
+            <td><?= $row['service_name'] ?></td>
+            <td><?= $row['cost'] ?></td>
+            <td><?= $row['service_date'] ?></td>
+        </tr>
+    <?php endwhile; ?>
+</table>
+
+<a href="v2_add.php">Add New Service</a>
+
