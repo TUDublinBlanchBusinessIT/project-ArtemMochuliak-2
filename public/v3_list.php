c0edece9f58c42628abb9039f2ac84cae1ae88e0
Natalia Lishchenko <B00165618@mytudublin.ie>
2025-11-29 18:28:54 +0000

    Updated Version 3 list page to display service type names using SQL JOIN

--- public/v3_list.php
+++ public/v3_list.php
@@ -11,8 +11,41 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = "SELECT * FROM basic_log_v3";
+$sql = "
+SELECT 
+    basic_log_v3.id,
+    service_types.type_name,
+    basic_log_v3.cost,
+    basic_log_v3.service_date
+FROM basic_log_v3
+JOIN service_types 
+    ON basic_log_v3.service_type_id = service_types.id
+";
+
 $result = mysqli_query($conn, $sql);
 
 $types = mysqli_query($conn, "SELECT * FROM service_types");
 ?>
+
+<h2>Service List (Version 3)</h2>
+
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Service Type ID</th>
+        <th>Cost (€)</th>
+        <th>Date</th>
+    </tr>
+
+    <?php while ($row = mysqli_fetch_assoc($result)): ?>
+        <tr>
+            <td><?= $row['id'] ?></td>
+            <td><?= $row['service_type_id'] ?></td>
+            <td><?= $row['cost'] ?></td>
+            <td><?= $row['service_date'] ?></td>
+        </tr>
+    <?php endwhile; ?>
+</table>
+
+<a href="v3_add.php">Add New Service</a>
+
