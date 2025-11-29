0eb4f9d158ced4133f623132da7537cb710ee95b
Natalia Lishchenko <B00165618@mytudublin.ie>
2025-11-29 18:47:01 +0000

    Created v5_delete.php to delete service records from Version 3 table

---
+++ public/v5_delete.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "PassB62tu";
+$dbname = "car_maintenance";
+$port = 3306;
+
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$id = $_GET['id']; 
+
+$sql = "DELETE FROM basic_log_v3 WHERE id = $id";
+
+mysqli_query($conn, $sql);
+
+header("Location: v3_list.php");
+exit;
+?>
